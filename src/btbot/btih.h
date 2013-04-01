bc2f5558b3ea42a4dba7542138f780f2f923cf79
andrew-aladev <aladjev.andrew@gmail.com>
2013-04-02 01:52:51 +0300

    implemented base32 decoding

--- src/btbot/btih.h
+++ src/btbot/btih.h
@@ -3,47 +3,83 @@
 // btbot is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Lesser Public License for more details.
 // You should have received a copy of the GNU General Lesser Public License along with btbot. If not, see <http://www.gnu.org/licenses/>.
 
+// See specification http://www.ietf.org/rfc/rfc4648.txt
+
 #ifndef BTIH_H
 #define BTIH_H
 
-#include <math.h>
 #include <string.h>
 #include <stdint.h>
 #include <talloc.h>
+#include <inttypes.h>
 
-static const int BASE32_SMALL_ALPHA_OFFSET = - 'a';
-static const int BASE32_BIG_ALPHA_OFFSET   = - 'A';
-static const int BASE32_NUMBER_OFFSET      = 26 - '2';
+#define __STDC_FORMAT_MACROS
+
+static const int BASE32_SMALL_ALPHA_OFFSET = 'a';
+static const int BASE32_BIG_ALPHA_OFFSET   = 'A';
+static const int BASE32_NUMBER_OFFSET      = '2' - 26;
 
 static inline
 int8_t from_base32 ( int8_t ch ) {
     if ( ch >= 'a' && ch <= 'z' ) {
-        return ch + BASE32_SMALL_ALPHA_OFFSET;
+        return ch - BASE32_SMALL_ALPHA_OFFSET;
     } else if ( ch >= 'A' && ch <= 'Z' ) {
-        return ch + BASE32_BIG_ALPHA_OFFSET;
+        return ch - BASE32_BIG_ALPHA_OFFSET;
     } else if ( ch >= '2' && ch <= '7' ) {
-        return ch + BASE32_NUMBER_OFFSET;
+        return ch - BASE32_NUMBER_OFFSET;
     } else {
         return - 1;
     }
 }
 
 extern inline
-char * bt_base32_to_base16 ( TALLOC_CTX * ctx, char * encoded ) {
-    size_t encoded_length = strlen ( encoded );
-    size_t result_length  = ( size_t ) ceil ( 1.25 * encoded_length );
-    char * result = talloc_array ( ctx, char, result_length );
-    if ( !result ) {
+uint8_t * bt_base32_decode ( TALLOC_CTX * ctx, char * src, size_t * result_length ) {
+    size_t src_length = strlen ( src );
+    if ( !src_length ) {
+        return NULL;
+    }
+
+    size_t bits = src_length * 5;
+    if ( bits % 8 ) {
+        // not properly padded string
         return NULL;
     }
 
     int8_t ch;
-    char * walk     = encoded;
-    char * walk_end = walk + encoded_length;
+    char * walk     = src;
+    char * walk_end = src + src_length;
+    do {
+        walk_end--;
+        ch = *walk_end;
+        // bypass padding
+        if ( ch != '=' ) {
+            break;
+        }
+
+        bits -= 5;
+    } while ( walk != walk_end );
+    walk_end++;
+
+    if ( !bits ) {
+        // all content is padding
+        return NULL;
+    }
+    size_t length = bits / 8;
+    if ( bits % 8 ) {
+        // should add zeroes to the right
+        length++;
+    }
 
     uint32_t buffer       = 0;
     uint8_t  buffer_size  = 0;
-    size_t   result_index = 0;
+
+    size_t result_index = 0;
+    uint8_t * result    = talloc_array ( ctx, uint8_t, length );
+    if ( !result ) {
+        return NULL;
+    }
+    *result_length = length;
+
     while ( walk != walk_end ) {
         buffer <<= 5;
         ch = from_base32 ( *walk );
@@ -54,21 +90,21 @@ char * bt_base32_to_base16 ( TALLOC_CTX * ctx, char * encoded ) {
         buffer |= ch;
         buffer_size += 5;
 
-        if ( buffer_size >= 28 ) {
-            while ( buffer_size ) {
-                ch = ( char ) buffer & 0xF;
-                //printf ( "%d\n", ( int ) ch );
-                buffer >>= 4;
-                result_index++;
-                buffer_size -= 4;
-            }
+        if ( buffer_size >= 8 ) {
+            result[result_index] = ( uint8_t ) ( buffer >> ( buffer_size - 8 ) );
+            buffer_size -= 8;
+            result_index++;
         }
 
         walk++;
     }
 
-    return NULL;
+    if ( buffer_size ) {
+        // adding zeroes to the right
+        result[result_index] = ( uint8_t ) buffer << ( 8 - buffer_size );
+    }
+
+    return result;
 }
 
 #endif
-
--- tests/02-btih.c
+++ tests/02-btih.c
@@ -15,10 +15,53 @@ int main() {
         talloc_free ( ctx );
         return 1;
     }
-    bt_base32_to_base16 ( ctx, "abcdefghijklmnopqrstuvwxqz234567" );
+    uint8_t * result;
+    size_t result_length = 0;
+
+    // should return NULL
+    result = bt_base32_decode ( ctx, "", &result_length );
+    if ( result ) {
+        talloc_free ( ctx );
+        return 2;
+    }
+    result = bt_base32_decode ( ctx, "=", &result_length );
+    if ( result ) {
+        talloc_free ( ctx );
+        return 3;
+    }
+    result = bt_base32_decode ( ctx, "========", &result_length );
+    if ( result ) {
+        talloc_free ( ctx );
+        return 4;
+    }
+    result = bt_base32_decode ( ctx, "=a======", &result_length );
+    if ( result ) {
+        talloc_free ( ctx );
+        return 5;
+    }
+    result = bt_base32_decode ( ctx, "a========", &result_length );
+    if ( result ) {
+        talloc_free ( ctx );
+        return 6;
+    }
+
+    // should return valid hash
+    result = bt_base32_decode ( ctx, "MFRGG===", &result_length );
+    uint8_t answer_1[] = {'a', 'b', 'c', 0};
+    if ( !result || result_length != 4 || memcmp ( result, answer_1, 4 ) ) {
+        talloc_free ( ctx );
+        return 7;
+    }
+    result = bt_base32_decode ( ctx, "BGQHKHR56P6J32HGQTCCHFIA7UTHWEF6", &result_length );
+    uint8_t answer_2[] = {0x09, 0xA0, 0x75, 0x1E, 0x3D, 0xF3, 0xFC, 0x9D, 0xE8, 0xE6, 0x84, 0xC4, 0x23, 0x95, 0x00, 0xFD, 0x26, 0x7B, 0x10, 0xBE, 0x00};
+    if ( !result || result_length != 20 || memcmp ( result, answer_2, 20 ) ) {
+        talloc_free ( ctx );
+        return 8;
+    }
 
     talloc_free ( ctx );
     return 0;
 }
+
 
 
