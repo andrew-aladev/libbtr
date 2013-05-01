e92db9a77143e9ee8ecdcaf26b7623eecd1ef2fe
andrew-aladev <aladjev.andrew@gmail.com>
2013-05-01 17:13:35 +0300

    btlib is already used. renamed to libbtr

--- src/btlib/utils/url.h
+++ src/btlib/utils/url.h
@@ -1,7 +1,7 @@
-// This file is part of btlib. Copyright (C) 2013 Andrew Aladjev aladjev.andrew@gmail.com
-// btlib is free software: you can redistribute it and/or modify it under the terms of the GNU General Lesser Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// btlib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Lesser Public License for more details.
-// You should have received a copy of the GNU General Lesser Public License along with btlib. If not, see <http://www.gnu.org/licenses/>.
+// This file is part of libbtr. Copyright (C) 2013 Andrew Aladjev aladjev.andrew@gmail.com
+// libbtr is free software: you can redistribute it and/or modify it under the terms of the GNU General Lesser Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// libbtr is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Lesser Public License for more details.
+// You should have received a copy of the GNU General Lesser Public License along with libbtr. If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef UTILS_URL_H
 #define UTILS_URL_H
--- tests/02-btih.c
+++ tests/02-btih.c
@@ -1,13 +1,13 @@
-// This file is part of btlib. Copyright (C) 2013 Andrew Aladjev aladjev.andrew@gmail.com
-// btlib is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// btlib is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License along with btlib. If not, see <http://www.gnu.org/licenses/>.
+// This file is part of libbtr. Copyright (C) 2013 Andrew Aladjev aladjev.andrew@gmail.com
+// libbtr is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// libbtr is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with libbtr. If not, see <http://www.gnu.org/licenses/>.
 
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <talloc.h>
-#include <btlib/btih.h>
+#include <libbtr/btih.h>
 
 int main() {
     TALLOC_CTX * ctx = talloc_new ( NULL );
